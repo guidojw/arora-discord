07c852ab139e6b511f9345bd897faac14cf50c0b
Guido de Jong <guido@guidojw.nl>
2020-03-30 16:49:33 +0200

    Include old and new role in promote command response (#36)

--- app/services/user.js
+++ app/services/user.js
@@ -13,7 +13,6 @@ exports.hasBadge = async (userId, badgeId) => {
     return (await applicationAdapter('get', `/v1/users/${userId}/has-badge/${badgeId}`)).data
 }
 
-exports.getUsers = async (userIds, excludeBannedMembers) => {
-    return (await applicationAdapter('post', '/v1/users', { userIds, excludeBannedMembers }))
-        .data
+exports.getUsers = async userIds => {
+    return (await applicationAdapter('post', '/v1/users', { userIds })).data
 }
