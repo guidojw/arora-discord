79912a84c362349702b755dceadfbd05777cfa9f
Guido de Jong <guido@guidojw.nl>
2020-04-27 00:21:38 +0200

    Feature/Server Booster Report (#51)
    * Implement job
    
    * Actually schedule job

--- config/cron.js
+++ config/cron.js
@@ -1,14 +1,19 @@
 'use strict'
 const saveVoteJob = require('../app/jobs/save-vote')
 const updateTimerJob = require('../app/jobs/update-timer')
+const premiumMembersReportJob = require('../app/jobs/premium-members-report')
 
 module.exports = {
-  'saveVoteJob': {
-    'expression': '*/2 * * * *',
-    'job': saveVoteJob
-  },
-  'updateTimerJob': {
-    'expression': '*/2 * * * *',
-    'job': updateTimerJob
-  }
+    saveVoteJob: {
+        expression: '*/2 * * * *',
+        job: saveVoteJob
+    },
+    updateTimerJob: {
+        expression: '*/2 * * * *',
+        job: updateTimerJob
+    },
+    premiumMembersReportJob: {
+        expression: '0 12 */1 * *',
+        job: premiumMembersReportJob
+    }
 }
