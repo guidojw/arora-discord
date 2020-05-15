46f0a8971f0627373355c293ef269dbe3f263d3b
Guido de Jong <guido@guidojw.nl>
2020-05-15 04:22:18 +0200

    Feature/Demote command (#61)
    * Add demote command
    
    * Change last scenario rank to undefined
    
    * Make get age and join date commands use new endpoints
    
    * Also allow errors to have msg instead of message field

--- app/commands/main/join-date.js
+++ app/commands/main/join-date.js
@@ -2,7 +2,6 @@
 const Command = require('../../controllers/command')
 const userService = require('../../services/user')
 const timeHelper = require('../../helpers/time')
-const applicationAdapter = require('../../adapters/application')
 const { MessageEmbed } = require('discord.js')
 
 const applicationConfig = require('../../../config/application')
@@ -30,11 +29,10 @@ module.exports = class JoinDateCommand extends Command {
         username = username ? typeof username === 'string' ? username : username.displayName : message.member
             .displayName
         const userId = await userService.getIdFromUsername(username)
-        const joinDate = new Date((await applicationAdapter('get', `/v1/users/${userId}/join-` +
-            'date')).data)
+        const user = await userService.getUser(userId)
         const embed = new MessageEmbed()
-            .addField(`${message.argString ? username: 'Your'} join date`, `${timeHelper.getDate(
-                joinDate)}`)
+            .addField(`${message.argString ? username: 'Your'} join date`, `${timeHelper.getDate(new Date(
+                user.created))}`)
             .setColor(applicationConfig.primaryColor)
         message.replyEmbed(embed)
     }
