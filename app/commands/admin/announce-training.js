6291c60d297716e3beaa9ccc772a656fd9805465
Guido de Jong <guido@guidojw.nl>
2020-05-25 05:41:01 +0200

    Feature/Automatic training shout (#64)
    * Remove medium parameter in announce training requests
    
    * Simplify embed construction

--- app/commands/admin/announce-training.js
+++ app/commands/admin/announce-training.js
@@ -11,42 +11,28 @@ module.exports = class AnnounceTrainingCommand extends Command {
         super(client, {
             group: 'admin',
             name: 'announcetraining',
-            details: 'TrainingId must be the ID of a currently scheduled training. Medium must be both, roblox or ' +
-            'discord.',
+            details: 'TrainingId must be the ID of a currently scheduled training.',
             aliases: ['announce'],
             description: 'Announces given training.',
-            examples: ['announce 1', 'announce 1 discord'],
+            examples: ['announce 1'],
             clientPermissions: ['SEND_MESSAGES'],
             args: [
                 {
                     key: 'trainingId',
                     type: 'integer',
                     prompt: 'Which training would you like to announce?'
-                },
-                {
-                    key: 'medium',
-                    type: 'string',
-                    prompt: 'Where would you like to announce this training?',
-                    default: 'both',
-                    oneOf: ['both', 'roblox', 'discord']
                 }
             ]
         })
     }
 
-    async execute (message, { trainingId, medium }) {
-        medium = medium.toLowerCase()
+    async execute (message, { trainingId }) {
         const authorId = await userService.getIdFromUsername(message.member.displayName)
-        const content = (await applicationAdapter('post', `/v1/groups/${applicationConfig
-            .groupId}/trainings/${trainingId}/announce`, { medium, authorId })).data
+        const announcement = (await applicationAdapter('post', `/v1/groups/${applicationConfig
+            .groupId}/trainings/${trainingId}/announce`, { authorId })).data
         const embed = new MessageEmbed()
             .setColor(applicationConfig.primaryColor)
-        if (medium === 'both' || medium === 'discord') {
-            embed.addField('Successfully announced', content.announcement)
-        }
-        if (medium === 'both' || medium === 'roblox') {
-            embed.addField('Successfully shouted', content.shout)
-        }
+            .addField('Successfully announced', announcement)
         message.replyEmbed(embed)
     }
 }
