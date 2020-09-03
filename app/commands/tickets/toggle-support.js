09100fd3a2bdfb3e3e4b6a1a64d07467c4d02dd5
guidojw <guidojwdejong@hotmail.com>
2020-09-03 03:58:28 +0200

    Implement command for toggling the support system

--- app/commands/tickets/toggle-support.js
+++ app/commands/tickets/toggle-support.js
@@ -1,6 +1,6 @@
 'use strict'
 const Command = require('../../controllers/command')
-const discordService = require('../../services/discord')
+const { MessageEmbed } = require('discord.js')
 
 module.exports = class ToggleSupportCommand extends Command {
     constructor (client) {
@@ -8,7 +8,7 @@ module.exports = class ToggleSupportCommand extends Command {
             group: 'tickets',
             name: 'togglesupport',
             aliases: ['toggle'],
-            description: 'Enables/disables the tickets system.',
+            description: 'Enables/disables the support system.',
             clientPermissions: ['MANAGE_MESSAGES', 'ADD_REACTIONS', 'SEND_MESSAGES'],
             guildOnly: false,
             adminOnly: true
@@ -16,6 +16,14 @@ module.exports = class ToggleSupportCommand extends Command {
     }
 
     async execute (message, _args, _guild) {
+        const settings = this.client.bot.mainGuild.getData('settings')
+        settings.supportEnabled = !settings.supportEnabled
+        this.client.bot.mainGuild.setData('settings', settings)
 
+        const embed = new MessageEmbed()
+            .setColor(settings.supportEnabled ? 0x00ff00 : 0xff0000)
+            .setTitle('Successfully toggled support')
+            .setDescription(`Tickets System: **${settings.supportEnabled ? 'enabled' : 'disabled'}**`)
+        message.replyEmbed(embed)
     }
 }
