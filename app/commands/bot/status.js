93196e8a373b426b8711805fffc4dc79944f8085
guidojw <guidojwdejong@hotmail.com>
2020-09-04 20:04:13 +0200

    Add status command

---
+++ app/commands/bot/status.js
@@ -0,0 +1,25 @@
+'use strict'
+const Command = require('../../controllers/command')
+const { MessageEmbed } = require('discord.js')
+
+module.exports = class StatusCommand extends Command {
+    constructor (client) {
+        super(client, {
+            group: 'bot',
+            name: 'status',
+            description: 'Posts the bot\'s systems\' statuses.',
+            clientPermissions: ['SEND_MESSAGES']
+        })
+    }
+
+    execute (message, _args, guild) {
+        const settings = guild.getData('settings')
+
+        const embed = new MessageEmbed()
+            .setColor(settings.supportEnabled ? 0x00ff00 : 0xff0000)
+            .setTitle('NSadmin status')
+            .setDescription(`Tickets System: **${settings.supportEnabled ? 'enabled' : 'disabled'}**`)
+            .setTimestamp()
+        message.replyEmbed(embed)
+    }
+}
