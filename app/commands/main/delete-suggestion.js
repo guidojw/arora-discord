4d59b4975afc81654d382c420319a5984c5aa421
guidojw <guidojwdejong@hotmail.com>
2020-11-28 02:50:20 +0100

    Remove DeleteSuggestion command message check

--- app/commands/main/delete-suggestion.js
+++ app/commands/main/delete-suggestion.js
@@ -15,14 +15,13 @@ module.exports = class DeleteSuggestionCommand extends Command {
 
   async execute (message, _args, guild) {
     const channels = guild.getData('channels')
-    const guildMessages = guild.getData('messages')
     const channel = guild.guild.channels.cache.get(channels.suggestionsChannel)
     const messages = await channel.messages.fetch()
     const authorUrl = `https://discordapp.com/users/${message.author.id}`
 
     for (const suggestion of messages.values()) {
       if (suggestion.embeds.length === 1 && suggestion.embeds[0].author && suggestion.embeds[0].author.url ===
-        authorUrl && suggestion.id !== guildMessages.firstSuggestionMessage) {
+        authorUrl) {
         const prompt = await message.replyEmbed(suggestion.embeds[0], 'Are you sure would like to delete this' +
           ' suggestion?')
         const choice = await discordService.prompt(message.channel, message.author, prompt, ['✅', '🚫']) ===
