8bb505a1c42b181b7e4e8b34f40546e99c164805
guidojw <guidojwdejong@hotmail.com>
2020-12-01 23:58:45 +0100

    Add database configurations

---
+++ config/database.js
@@ -0,0 +1,31 @@
+'use strict'
+module.exports = {
+  development: {
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    host: '127.0.0.1',
+    database: 'nsadmin_discord_development',
+
+    dialect: 'postgres',
+    operatorsAliases: '0',
+    define: {
+      timestamps: false,
+      underscored: true
+    }
+  },
+
+  production: {
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    host: process.env.POSTGRES_HOST,
+    database: 'nsadmin_discord_production',
+
+    dialect: 'postgres',
+    operatorsAliases: '0',
+    define: {
+      timestamps: false,
+      underscored: true
+    },
+    logging: false
+  }
+}
