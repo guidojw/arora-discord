e0d4d9035497f9d42c59d913e30e78b4b69f1e5c
guidojw <guidojwdejong@hotmail.com>
2020-12-17 04:56:19 +0100

    Allow settings to be set to null

--- app/commands/settings/set-setting.js
+++ app/commands/settings/set-setting.js
@@ -23,7 +23,7 @@ module.exports = class SetSettingCommand extends Command {
       }, {
         key: 'value',
         prompt: 'What would you like to change this setting to?',
-        type: 'category-channel|channel|message|integer'
+        type: 'category-channel|channel|message|integer|string'
       }]
     })
   }
@@ -36,38 +36,43 @@ module.exports = class SetSettingCommand extends Command {
         return attribute.endsWith('id') ? attribute.slice(0, -2) === key : attribute === key
       })
 
-    let error
-    if (key === 'primaryColor') {
-      if (typeof value !== 'number') {
-        value = parseInt(value, 16)
-        if (isNaN(value)) {
-          error = 'Invalid color.'
-        }
-      } else if (value < 0 || value > 16777215) { // [0x000000, 0xffffff]
-        error = 'Color out of bounds.'
-      }
-    } else if (key === 'robloxGroupId') {
-      if (typeof value !== 'number') {
-        error = 'Invalid ID.'
-      }
-    } else if (key === 'ticketsCategoryId') {
-      if (!(value instanceof CategoryChannel)) {
-        error = 'Invalid category channel.'
-      }
-    } else if (key === 'trainingsMessageId' || key === 'trainingsInfoMessageId') {
-      if (!(value instanceof Message)) {
-        error = 'Invalid message.'
-      }
+    if (typeof value === 'string' && value === 'null') {
+      value = null
     } else {
-      if (!(value instanceof Channel)) {
-        error = 'Invalid channel.'
+      let error
+      if (key === 'primaryColor') {
+        if (typeof value !== 'number') {
+          value = parseInt(value, 16)
+          if (isNaN(value)) {
+            error = 'Invalid color.'
+          }
+        } else if (value < 0 || value > 16777215) { // [0x000000, 0xffffff]
+          error = 'Color out of bounds.'
+        }
+      } else if (key === 'robloxGroupId') {
+        if (typeof value !== 'number') {
+          error = 'Invalid ID.'
+        }
+      } else if (key === 'ticketsCategoryId') {
+        if (!(value instanceof CategoryChannel)) {
+          error = 'Invalid category channel.'
+        }
+      } else if (key === 'trainingsMessageId' || key === 'trainingsInfoMessageId') {
+        if (!(value instanceof Message)) {
+          error = 'Invalid message.'
+        }
+      } else {
+        if (!(value instanceof Channel)) {
+          error = 'Invalid channel.'
+        }
       }
-    }
-    if (error) {
-      return message.reply(error)
+
+      if (error) {
+        return message.reply(error)
+      }
     }
 
-    await guild.edit({ [key]: key.endsWith('Id') && key !== 'robloxGroupId' ? value.id : value })
+    await guild.edit({ [key]: value !== null && key.endsWith('Id') && key !== 'robloxGroupId' ? value.id : value })
 
     return message.reply(`Successfully changed ${key.endsWith('Id') ? key.slice(0, -2) : key} to **${value}**.`)
   }
