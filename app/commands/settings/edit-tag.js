e5113ee21cbf5eb07c43f26c07a8f3bdeeaf017b
guidojw <guidojwdejong@hotmail.com>
2020-12-26 05:40:46 +0100

    Add command for editing tags

---
+++ app/commands/settings/edit-tag.js
@@ -0,0 +1,46 @@
+'use strict'
+const Command = require('../../controllers/command')
+
+const { Tag, TagName } = require('../../models')
+
+class EditTagCommand extends Command {
+  constructor (client) {
+    super(client, {
+      group: 'settings',
+      name: 'edittag',
+      description: 'Edits a tag.',
+      clientPermissions: ['SEND_MESSAGES'],
+      args: [{
+        key: 'name',
+        prompt: 'What tag would you like to edit?',
+        type: 'string',
+        validate: _validateName
+      }, {
+        key: 'content',
+        prompt: 'What do you want the new content of this tag to be?',
+        type: 'string'
+      }]
+    })
+  }
+
+  async execute (message, { name, content }, guild) {
+    name = name.toLowerCase()
+    const tag = await Tag.findOne({
+      where: { guildId: guild.id },
+      include: [{ model: TagName, as: 'names', where: { name } }]
+    })
+    if (!tag) {
+      return message.reply('Tag not found.')
+    }
+
+    await tag.edit({ content })
+
+    return message.reply(`Successfully edited tag **${tag.names[0]?.name ?? 'Unknown'}**.`)
+  }
+}
+
+function _validateName (name) {
+  return name.includes(' ') ? 'Name cannot include spaces.' : true
+}
+
+module.exports = EditTagCommand
