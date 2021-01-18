c2bb99b16e856a494184aa337be57d5f27360e9e
guidojw <guidojwdejong@hotmail.com>
2021-01-18 08:54:52 +0100

    Rename methods in WebSocketManager

--- app/client/websocket/websocket.js
+++ app/client/websocket/websocket.js
@@ -10,42 +10,46 @@ class WebSocketManager {
   }
 
   connect () {
-    this.connection = new WebSocket(`${this.host}/v1`, {
+    this.ws = new WebSocket(`${this.host}/v1`, {
       headers: {
         Authorization: `Bearer ${process.env.TOKEN}`
       }
     })
 
-    this.connection.on('open', this.open.bind(this))
-    this.connection.on('error', console.error)
-    this.connection.on('close', this.close.bind(this))
-    this.connection.on('ping', this.ping.bind(this))
-    this.connection.on('message', this.message.bind(this))
+    this.ws.on('open', this.onOpen.bind(this))
+    this.ws.on('message', this.onMessage.bind(this))
+    this.ws.on('close', this.onClose.bind(this))
+    this.ws.on('error', this.onError.bind(this))
+    this.ws.on('ping', this.onPing.bind(this))
   }
 
-  heartbeat () {
-    clearTimeout(this.pingTimeout)
-    this.pingTimeout = setTimeout(this.connection.terminate, 30000 + 1000)
-  }
-
-  open () {
+  onOpen () {
     console.log('Connected!')
     this.heartbeat()
   }
 
-  close () {
+  onMessage (message) {
+    const packet = JSON.parse(message)
+    this.handlePacket(packet)
+  }
+
+  onClose () {
     console.log('Disconnected!')
     clearTimeout(this.pingTimeout)
     setTimeout(this.connect.bind(this), 30000)
   }
 
-  ping () {
+  onError (error) {
+    this.client.emit('webhookError', error)
+  }
+
+  onPing () {
     this.heartbeat()
   }
 
-  message (message) {
-    const packet = JSON.parse(message)
-    this.handlePacket(packet)
+  heartbeat () {
+    clearTimeout(this.pingTimeout)
+    this.pingTimeout = setTimeout(this.ws.terminate, 30000 + 1000)
   }
 
   handlePacket (packet) {
