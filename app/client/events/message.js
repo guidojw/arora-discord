bd1a4e2f8d2e8c5b3f3ead7f7749b7e5fdcc064c
guidojw <guidojwdejong@hotmail.com>
2021-01-23 06:14:16 +0100

    Also respect group state on message tag

--- app/client/events/message.js
+++ app/client/events/message.js
@@ -12,11 +12,12 @@ class MessageEvent extends BaseEvent {
       return
     }
     const guild = this.client.guilds.cache.get(message.guild.id)
-    const prefix = guild.commandPrefix ?? this.commandPrefix
 
     const tagCommand = this.client.registry.commands.find(command => command.name === 'tag')
-    if (tagCommand.isEnabledIn(message.guild) && message.content.startsWith(prefix)) {
-      const args = message.content.slice(prefix.length).trim().split(/ +/)
+    const mainGroup = this.client.registry.groups.find(group => group.name === 'Main')
+    if (tagCommand.isEnabledIn(message.guild) && mainGroup.isEnabledIn(message.guild) &&
+      message.content.startsWith(guild.commandPrefix)) {
+      const args = message.content.slice(guild.commandPrefix.length).trim().split(/ +/)
       const tagName = args.shift().toLowerCase()
       if (tagName) {
         const tag = await Tag.findOne({
