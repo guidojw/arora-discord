48c71b2f3a42c5d1b83492582f5ff0d17e7d8b34
guidojw <guidojwdejong@hotmail.com>
2021-03-22 07:11:59 +0100

    Don't log on commandError

--- app/client/events/command-error.js
+++ app/client/events/command-error.js
@@ -1,6 +1,4 @@
 'use strict'
-const { stripIndents } = require('common-tags')
-
 const commandErrorHandler = (client, command, err, message, _args, _fromPattern, collResult) => {
   if (err.response?.data.errors?.length > 0) {
     message.reply(err.response.data.errors[0].message ?? err.response.data.errors[0].msg)
@@ -10,17 +8,6 @@ const commandErrorHandler = (client, command, err, message, _args, _fromPattern,
 
   collResult?.prompts.forEach(client.deleteMessage.bind(client))
   collResult?.answers.forEach(client.deleteMessage.bind(client))
-
-  const guild = message.guild || client.mainGuild
-  guild.log(
-    message.author,
-    stripIndents`
-    ${message.author} **used** \`${command.name}\` **command in** ${message.channel} [Jump to Message](${message.url})
-    ${message.content}
-    `, {
-      color: 0xff0000
-    }
-  )
 }
 
 module.exports = commandErrorHandler
