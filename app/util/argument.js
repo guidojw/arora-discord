25827e87ca64043775498b0adb7dacf14ed78e62
guidojw <guidojwdejong@hotmail.com>
2021-03-24 23:15:20 +0100

    Disable eslint max-len on RegExps

--- app/util/argument.js
+++ app/util/argument.js
@@ -1,7 +1,6 @@
 'use strict'
 const { MessageMentions } = require('discord.js')
 
-// eslint-disable-next-line max-len
 const urlRegex = /(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})/gi
 
 function validators (steps = []) {
