d30acbb3845c223e094572146956b2aa01599d2e
guidojw <guidojwdejong@hotmail.com>
2021-03-28 23:07:31 +0200

    chore(*): style changes models

--- app/models/group.js
+++ app/models/group.js
@@ -1,4 +1,5 @@
 'use strict'
+
 const { GroupTypes } = require('../util').Constants
 
 module.exports = (sequelize, DataTypes) => {
--- app/models/role.js
+++ app/models/role.js
@@ -1,4 +1,5 @@
 'use strict'
+
 module.exports = (sequelize, DataTypes) => {
   const Role = sequelize.define('Role', {
     id: {
