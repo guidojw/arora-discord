17cb7ec5571157d99f29faaaa1304f789af1d209
guidojw <guidojwdejong@hotmail.com>
2021-03-28 23:03:51 +0200

    chore(*): style changes extensions

--- app/extensions/guild-member.js
+++ app/extensions/guild-member.js
@@ -1,4 +1,5 @@
 'use strict'
+
 const { Structures } = require('discord.js')
 const { Member, Role } = require('../models')
 
--- app/extensions/voice-channel.js
+++ app/extensions/voice-channel.js
@@ -1,4 +1,5 @@
 'use strict'
+
 const { Structures } = require('discord.js')
 const { Channel } = require('../models')
 
