e1bd7d1d3ff8772a94e0cd696748ab9134bddf9b
guidojw <guidojwdejong@hotmail.com>
2021-03-31 05:12:04 +0200

    feat: bloxlinkAdapter

---
+++ app/adapters/bloxlink.js
@@ -0,0 +1,12 @@
+'use strict'
+
+const axios = require('axios')
+
+function bloxlinkAdapter (method, pathname) {
+  return axios({
+    url: 'https://api.blox.link/v1' + pathname,
+    method
+  })
+}
+
+module.exports = bloxlinkAdapter
