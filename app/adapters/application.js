94dab13cf8cd5859b00f0addb077fd8a5d35869e
guidojw <guidojwdejong@hotmail.com>
2021-04-02 19:17:56 +0200

    revert: feat(applicationAdapter): remove API check
    This reverts commit 11162a45

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -2,15 +2,21 @@
 
 const axios = require('axios')
 
+const applicationConfig = require('../../config/application')
+
 function applicationAdapter (method, pathname, data) {
-  return axios({
-    url: process.env.HOST + pathname,
-    method,
-    data,
-    headers: {
-      Authorization: `Bearer ${process.env.TOKEN}`
-    }
-  })
+  if (applicationConfig.apiEnabled) {
+    return axios({
+      url: process.env.HOST + pathname,
+      method,
+      data,
+      headers: {
+        Authorization: `Bearer ${process.env.TOKEN}`
+      }
+    })
+  } else {
+    throw new Error('This bot has no API enabled.')
+  }
 }
 
 module.exports = applicationAdapter
