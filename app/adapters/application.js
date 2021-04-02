11162a455a2258d5dc7ebb5dda8de7e8d6f096f3
guidojw <guidojwdejong@hotmail.com>
2021-04-02 18:59:02 +0200

    feat(applicationAdapter): remove API check

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -2,21 +2,15 @@
 
 const axios = require('axios')
 
-const applicationConfig = require('../../config/application')
-
 function applicationAdapter (method, pathname, data) {
-  if (applicationConfig.apiEnabled) {
-    return axios({
-      url: process.env.HOST + pathname,
-      method,
-      data,
-      headers: {
-        Authorization: `Bearer ${process.env.TOKEN}`
-      }
-    })
-  } else {
-    throw new Error('This bot has no API enabled.')
-  }
+  return axios({
+    url: process.env.HOST + pathname,
+    method,
+    data,
+    headers: {
+      Authorization: `Bearer ${process.env.TOKEN}`
+    }
+  })
 }
 
 module.exports = applicationAdapter
