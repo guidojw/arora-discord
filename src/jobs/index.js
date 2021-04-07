6a404e9db92a71d9b382a0ecf8a017c2d3dacad9
Guido de Jong <guidojwdejong@hotmail.com>
2021-04-07 04:07:33 +0200

    feat: implement job for service health checks (#239)

---
+++ src/jobs/index.js
@@ -0,0 +1,7 @@
+'use strict'
+
+module.exports = {
+  announceTrainingsJob: require('./announce-trainings'),
+  healthCheckJob: require('./health-check'),
+  premiumMembersReportJob: require('./premium-members-report')
+}
