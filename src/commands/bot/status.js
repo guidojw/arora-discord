025f890f4e1a5b0ed4a58f634f6ec8de3a07cff8
Guido de Jong <guido@guidojw.nl>
2021-04-18 02:05:38 +0200

    feat: add API stats to stats command output (#266)

--- src/commands/bot/status.js
+++ src/commands/bot/status.js
@@ -4,8 +4,11 @@ const os = require('os')
 const BaseCommand = require('../base')
 
 const { MessageEmbed } = require('discord.js')
+const { applicationAdapter } = require('../../adapters')
 const { getDurationString } = require('../../util').timeUtil
 const { formatBytes } = require('../../util').util
+
+const applicationConfig = require('../../../config/application')
 
 class StatusCommand extends BaseCommand {
   constructor (client) {
@@ -18,16 +21,27 @@ class StatusCommand extends BaseCommand {
     })
   }
 
-  run (message) {
+  async run (message) {
     const embed = new MessageEmbed()
       .setAuthor(this.client.user.username, this.client.user.displayAvatarURL())
       .setColor(0xff82d1)
-      .addField('System Statuses', `Tickets System: **${message.guild.supportEnabled ? 'online' : 'offline'}**`)
+    if (message.guild) {
+      embed.addField('System Statuses', `Tickets System: **${message.guild.supportEnabled ? 'online' : 'offline'}**`)
+    }
+    embed
       .addField('Load Average', os.loadavg().join(', '), true)
       .addField('Memory Usage', `${formatBytes(os.freemem(), 3)} / ${formatBytes(os.totalmem(), 3)}`, true)
       .addField('Uptime', getDurationString(this.client.uptime), true)
       .setFooter(`Process ID: ${process.pid} | ${os.hostname()}`)
       .setTimestamp()
+    if (applicationConfig.apiEnabled) {
+      const startTime = Date.now()
+      const status = (await applicationAdapter('GET', 'v1/status')).data
+      const endTime = Date.now()
+      embed
+        .addField('API Latency', (endTime - startTime) + 'ms', true)
+        .addField('API Status', status.state, true)
+    }
     return message.replyEmbed(embed)
   }
 }
