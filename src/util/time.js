90a4ce4340593363b62b439e55e3daa303b145f6
Guido de Jong <guido@guidojw.nl>
2021-05-02 00:40:33 +0200

    fix: accept hour 24 in timezone regex (#276)

--- src/util/time.js
+++ src/util/time.js
@@ -55,7 +55,7 @@ function getTimeInfo (timeString) {
 
 function getTimeZoneAbbreviation (date) {
   return date.toLocaleTimeString('en-us', { hour12: false, hour: '2-digit', minute: '2-digit', timeZoneName: 'long' })
-    .replace(/^(2[0-3]|[0-1]?\d):[0-5]\d\s/, '')
+    .replace(/^(2[0-4]|[0-1][1-9]):[0-5]\d\s/, '')
     .split(' ')
     .filter(word => word !== 'Standard')
     .map(word => word.charAt(0))
