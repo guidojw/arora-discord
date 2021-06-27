87f7879297adabfcb418896d157c3c0ff3603839
guidojw <guidojwdejong@hotmail.com>
2021-06-27 17:08:08 +0200

    fix: correct table name and add emoji validation

--- src/entities/ticket-type.ts
+++ src/entities/ticket-type.ts
@@ -1,13 +1,13 @@
 import { Column, Entity, JoinColumn, ManyToOne, OneToMany, OneToOne, PrimaryGeneratedColumn } from 'typeorm'
 import { Expose, Type } from 'class-transformer'
+import { IsNotEmpty, ValidateIf } from 'class-validator'
 // import Emoji from './emoji'
 import Guild from './guild'
-import { IsNotEmpty } from 'class-validator'
 import Message from './message'
 import { Nand } from '../util/util'
 import Ticket from './ticket'
 
-@Entity({ name: 'ticket-types' })
+@Entity({ name: 'ticket_types' })
 export default class TicketType {
   @Expose()
   @PrimaryGeneratedColumn()
@@ -20,7 +20,9 @@ export default class TicketType {
 
   @Expose()
   @Column('varchar', { length: 7, nullable: true })
+  @ValidateIf(ticketType => ticketType.emoji != null)
   @Nand('emojiId')
+  @IsNotEmpty()
   public emoji?: string | null
 
   @Expose({ name: 'guild_id' })
