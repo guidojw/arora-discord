36bee60af28965b674a5758753ecc04447da39fe
guidojw <guidojwdejong@hotmail.com>
2021-08-21 21:29:53 +0200

    refactor: move initialisation logic to loaders

--- src/loaders/index.ts
+++ src/loaders/index.ts
@@ -1,6 +1,36 @@
-import type { Container } from 'inversify'
+import * as Sentry from '@sentry/node'
+import AroraClient from '../client/client'
+import type { BaseJob } from '../jobs'
+import { constants } from '../util'
 import containerLoader from './container'
+import cron from 'node-cron'
+import cronConfig from '../configs/cron'
 
-export default async function init (): Promise<Container> {
-  return await containerLoader()
+const { TYPES } = constants
+
+export async function init (): Promise<AroraClient> {
+  if (typeof process.env.SENTRY_DSN !== 'undefined') {
+    Sentry.init({
+      dsn: process.env.SENTRY_DSN,
+      environment: process.env.NODE_ENV,
+      release: process.env.BUILD_HASH
+    })
+  }
+
+  const container = await containerLoader()
+
+  const jobFactory = container.get<(jobName: string) => BaseJob>(TYPES.JobFactory)
+  const healthCheckJobConfig = cronConfig.healthCheckJobConfig
+  const healthCheckJob = jobFactory(healthCheckJobConfig.name)
+  cron.schedule(
+    healthCheckJobConfig.expression,
+    healthCheckJob.run.bind(healthCheckJob.run, 'main')
+  )
+
+  const client = new AroraClient({
+    commandEditableDuration: 0
+  })
+  await client.login(process.env.DISCORD_TOKEN)
+
+  return client
 }
