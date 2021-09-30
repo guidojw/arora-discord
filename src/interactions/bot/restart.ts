aeb1ca90d01ec2193b11fae04ae4dc5cd6b1cd48
guidojw <guidojwdejong@hotmail.com>
2021-10-01 00:20:20 +0200

    feat(botCommands): write interaction objects

---
+++ src/interactions/bot/restart.ts
@@ -0,0 +1,7 @@
+const restartCommand = {
+  name: 'restart',
+  description: 'Restarts the bot',
+  defaultPermission: false
+}
+
+export default restartCommand
---
+++ src/interactions/bot/status.ts
@@ -0,0 +1,6 @@
+const statusCommand = {
+  name: 'status',
+  description: 'Posts the bot\'s system statuses'
+}
+
+export default statusCommand
