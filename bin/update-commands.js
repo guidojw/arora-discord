22daad2a69355ada0ebea81761ef0559a673d4b8
guidojw <guidojwdejong@hotmail.com>
2021-10-05 02:35:27 +0200

    feat(bin): make update commands script

---
+++ bin/update-commands.js
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+'use strict'
+
+require('dotenv').config()
+
+const { REST } = require('@discordjs/rest')
+const { Routes } = require('discord-api-types/v9')
+const interactions = require('../dist/interactions')
+
+async function updateCommands () {
+  const rest = new REST({ version: '9' }).setToken(process.env.DISCORD_TOKEN)
+  const application = await rest.get(Routes.oauth2CurrentApplication())
+  await rest.put(Routes.applicationCommands(application.id), { body: interactions })
+}
+
+updateCommands()
