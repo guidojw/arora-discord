9f12c60b822d57c8b6ff6364923754004695345a
guidojw <guidojwdejong@hotmail.com>
2021-11-28 23:52:44 +0100

    fix: comment consistent type imports rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -26,7 +26,7 @@ module.exports = {
     'no-duplicate-imports': 'error',
     'sort-imports': 'error',
     'unicorn/prefer-node-protocol': 'error',
-    '@typescript-eslint/consistent-type-imports': 'error',
+    // '@typescript-eslint/consistent-type-imports': 'error',
     '@typescript-eslint/explicit-member-accessibility': 'error'
   }
 }
