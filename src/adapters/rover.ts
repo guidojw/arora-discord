f241bb8ec65fd72f77a5619eecb779ad8c0ce1d5
Guido de Jong <35309288+guidojw@users.noreply.github.com>
2022-10-22 22:18:48 +0200

    refactor: support new RoVer API (#504)

--- src/adapters/rover.ts
+++ src/adapters/rover.ts
@@ -1,8 +1,12 @@
-import axios, { type AxiosPromise, type Method } from 'axios'
+import axios, { type AxiosPromise, type AxiosRequestConfig, type Method } from 'axios'
 
 export default async function roVerAdapter (method: Method, pathname: string): Promise<AxiosPromise> {
-  return await axios({
-    url: 'https://verify.eryn.io/api/' + pathname,
+  const options: AxiosRequestConfig = {
+    url: 'https://registry.rover.link/api/' + pathname,
     method
-  })
+  }
+  if (typeof process.env.ROVER_TOKEN !== 'undefined') {
+    options.headers = { Authorization: `Bearer ${process.env.ROVER_TOKEN}` }
+  }
+  return await axios(options)
 }
