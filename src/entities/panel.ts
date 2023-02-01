4cc846ec7e646e4564b1d48eb16abce3293d4960
Guido de Jong <35309288+guidojw@users.noreply.github.com>
2023-02-02 00:10:53 +0100

    fix: explicitly specify all class-validator validations (#535)

--- src/entities/panel.ts
+++ src/entities/panel.ts
@@ -1,29 +1,43 @@
 import { Column, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from 'typeorm'
+import { IsNumber, IsNumberString, IsOptional, IsString, MaxLength, ValidateIf, ValidateNested } from 'class-validator'
 import Guild from './guild'
 import Message from './message'
 
 @Entity('panels')
 export default class Panel {
   @PrimaryGeneratedColumn()
+  @ValidateIf(panel => typeof panel.id !== 'undefined')
+  @IsNumber({ maxDecimalPlaces: 0 })
   public readonly id!: number
 
   @Column({ length: 255 })
+  @IsString()
+  @MaxLength(255)
   public name!: string
 
   @Column({ length: 7000 })
+  @IsString()
+  @MaxLength(7000)
   public content!: string
 
   @Column('bigint', { name: 'guild_id' })
+  @IsNumberString({ no_symbols: true })
   public guildId!: string
 
   @Column('bigint', { name: 'message_id', nullable: true, unique: true })
+  @IsOptional()
+  @IsString()
   public messageId?: string | null
 
   @ManyToOne(() => Guild, guild => guild.panels, { onDelete: 'CASCADE' })
   @JoinColumn({ name: 'guild_id' })
+  @ValidateIf(panel => typeof panel.guild !== 'undefined')
+  @ValidateNested()
   public guild?: Guild
 
   @ManyToOne(() => Message, message => message.panel, { onDelete: 'SET NULL' })
   @JoinColumn({ name: 'message_id' })
+  @IsOptional()
+  @ValidateNested()
   public message?: Message | null
 }
--- src/entities/tag-name.ts
+++ src/entities/tag-name.ts
@@ -1,19 +1,24 @@
 import { Column, Entity, JoinColumn, ManyToOne, PrimaryColumn } from 'typeorm'
-import { IsLowercase, IsNotEmpty } from 'class-validator'
+import { IsLowercase, IsNotEmpty, IsNumber, IsString, MaxLength, ValidateIf, ValidateNested } from 'class-validator'
 import Tag from './tag'
 
 @Entity('tag_names')
 export default class TagName {
   @PrimaryColumn({ length: 255 })
+  @IsString()
   @IsNotEmpty()
   @IsLowercase()
+  @MaxLength(255)
   public name!: string
 
   @Column({ name: 'tag_id' })
+  @IsNumber({ maxDecimalPlaces: 0 })
   public tagId!: number
 
   @ManyToOne(() => Tag, tag => tag.names, { onDelete: 'CASCADE' })
   @JoinColumn({ name: 'tag_id' })
+  @ValidateIf(tagName => typeof tagName.tag !== 'undefined')
+  @ValidateNested()
   public tag?: Tag
 
   public get id (): string {
