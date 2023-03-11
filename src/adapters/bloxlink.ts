0fedb0226e15529b73ba8e899af32054b3be0ce7
guidojw <35309288+guidojw@users.noreply.github.com>
2023-03-11 21:11:01 +0100

    refactor: support new Bloxlink API

--- src/adapters/bloxlink.ts
+++ src/adapters/bloxlink.ts
@@ -1,8 +1,12 @@
-import axios, { type AxiosPromise, type Method } from 'axios'
+import axios, { type AxiosPromise, type AxiosRequestConfig, type Method } from 'axios'
 
 export default async function bloxlinkAdapter (method: Method, pathname: string): Promise<AxiosPromise> {
-  return await axios({
-    url: 'https://api.blox.link/v1/' + pathname,
+  const options: AxiosRequestConfig = {
+    url: 'https://v3.blox.link/developer/discord/' + pathname,
     method
-  })
+  }
+  if (typeof process.env.BLOXLINK_TOKEN !== 'undefined') {
+    options.headers = { 'api-key': process.env.BLOXLINK_TOKEN }
+  }
+  return await axios(options)
 }
