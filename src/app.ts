e7397d1fab26d7d3914d017ee18b183a7c66155b
guidojw <35309288+guidojw@users.noreply.github.com>
2026-05-10 19:12:46 +0200

    fix(app): reorder imports

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,10 @@
 import 'reflect-metadata'
-import * as loaders from './loaders'
 import dotenv from 'dotenv'
 
 dotenv.config()
+
+// eslint-disable-next-line max-len
+// eslint-disable-next-line import/first, sort-imports -- Sentry loader needs dotenv configured first.
+import * as loaders from './loaders'
 
 export default loaders.init()
